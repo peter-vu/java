088ec7eacfe9c86749557adda9ea836a0c20dc04
Vu Tuan Anh <vutuananh.vn.sg@gmail.com>
2020-03-01 22:34:32 +0800

    commit

--- springinaction/readinglist/src/main/java/com/manning/readlinglist/SecurityConfig.java
+++ springinaction/readinglist/src/main/java/com/manning/readlinglist/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-			.antMatchers("/").access("hasRole('READER'")
+			.antMatchers("/").access("hasRole('READER')")
 			.antMatchers("/**").permitAll()
 			
 		.and()
