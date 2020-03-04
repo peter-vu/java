42282b3a751ffac42d241dcf29c5a43f9fd20401
Vu Tuan Anh <vutuananh.vn.sg@gmail.com>
2020-03-04 23:20:19 +0800

    config error page

--- springinaction/readinglist/src/main/java/com/manning/readlinglist/ReadingListController.java
+++ springinaction/readinglist/src/main/java/com/manning/readlinglist/ReadingListController.java
@@ -3,6 +3,7 @@ package com.manning.readlinglist;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,35 +12,44 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/")
+@ConfigurationProperties(prefix="amazon")
 public class ReadingListController {
+	
+	private AmazonProperties amazonProperties;
+	
 	private ReadingListRepository readingListRepository;
 	
 	@Autowired
-	public ReadingListController(ReadingListRepository repository) {
+	public ReadingListController(ReadingListRepository repository
+			,AmazonProperties amazonProperties) {
 		this.readingListRepository = repository;
+		this.amazonProperties = amazonProperties;
 	}
 	
-	@RequestMapping(value="/{reader}", method=RequestMethod.GET)
+	@RequestMapping(method=RequestMethod.GET)
 	public String readersBooks(
-			@PathVariable("reader") String reader,
+			Reader reader,
 			Model model) {
 		List<Book> readingList = readingListRepository.findByReader(reader);
 		if(readingList != null) {
 			model.addAttribute("books", readingList);
+			model.addAttribute("reader", reader);
+			model.addAttribute("amazonID", amazonProperties.getAssociateId());
 		}
+		
 		return "readingList";
 	}
 	
-	@RequestMapping(value="/{reader}", method=RequestMethod.POST)
+	@RequestMapping(method=RequestMethod.POST)
 	public String addToReadingList(
 			@PathVariable("reader") String reader,
 			Book book) {
 		book.setReader(reader);
 		readingListRepository.save(book);
-		return "redirect:/{reader}";
+		return "redirect:/";
 		
 	}
-		
+
 	
 	
 }
